7049ce380bf48a8ee578062defb5eca75faf6ebc
hkdeep148 <106611278+hkdeep148@users.noreply.github.com>
2026-09-10 13:02:05 +0530

    Test complete WZATCO Source raw sequence
    Updated wz_source_raw array to include additional values and changed the length parameter in sendRaw function.

--- src/driver/drv_ir_new.cpp
+++ src/driver/drv_ir_new.cpp
@@ -356,17 +356,18 @@ extern "C" void DRV_IR_ISR(void* arg)
 	ir_counter++;
 }
 
-static const uint16_t wz_source_raw[67] = {
-    8640,4272,
-    480,528,480,1584,576,1488,
-    336,480,480,480,576,480,384,528,
-    480,1584,576,1536,432,1536,576,480,
-    576,1488,576,480,144,1488,384,1392,
-    480,480,528,1536,576,1536,528,528,
-    528,1440,576,528,576,528,528,528,
-    576,528,528,528,480,480,480,1488,
-    528,336,336,1632,528,1632,528,1584,
-    576,1536,576
+static const uint16_t wz_source_raw[71] = {
+    8400,3840,
+    528,528,576,1584,576,1584,
+    576,480,576,528,576,480,576,528,
+    576,1488,576,1584,576,1536,576,480,
+    576,1584,576,528,576,1584,528,1488,
+    528,576,528,1632,528,1584,576,528,
+    528,1632,480,576,528,528,528,576,
+    528,528,528,576,528,576,528,1584,
+    576,528,528,1584,576,1584,576,1584,
+    576,1584,576,
+    37776,8544,2112,576
 };
 
 extern "C" commandResult_t IR_Send_Raw_Test(const void *context, const char *cmd, const char *args_in, int cmdFlags) {
@@ -375,7 +376,7 @@ extern "C" commandResult_t IR_Send_Raw_Test(const void *context, const char *cmd
         return CMD_RES_ERROR;
     }
 
-    pIRsend->sendRaw(wz_source_raw, 67, 38);
+    pIRsend->sendRaw(wz_source_raw, 71, 38);
     pIRsend->delay(100);
 
     ADDLOG_INFO(LOG_FEATURE_IR, (char *)"IR raw test sent WZATCO Source");
